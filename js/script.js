eb8db2f7c087666ddb4f8266165c4529ba4e3863
Ivan Norderhaug <ivann@stud.ntnu.no>
2022-03-11 01:34:53 +0100

    Added hidden secret

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function easterEgg(){
+	terminal.remove();
+	document.getElementById("secret").style.visibility = "visible";
+}
+
+function wait(){
+	let x = document.querySelector(".typewriter h1");
+	x.setAttribute('style', 'border-right: 0.8em transparent');
+}
